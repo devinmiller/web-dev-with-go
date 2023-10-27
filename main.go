cdf29fc146a997337ab12830859fd4bd1b90bc4b
Devin Miller <devinmiller@codeonthebrain.com>
2023-10-27 16:53:14 -0700

    mongo db connection

--- main.go
+++ main.go
@@ -1,26 +1,63 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/devinmiller/web-dev-with-go/controllers"
 	"github.com/devinmiller/web-dev-with-go/templates"
 	"github.com/devinmiller/web-dev-with-go/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func initDatabase(uri string) (client *mongo.Client, err error) {
+	fmt.Println("Connecting to database...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func endConnection(client *mongo.Client) {
+	fmt.Println("Disconnecting from database...")
+
+	if err := client.Disconnect(context.Background()); err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	fmt.Println("Starting server on :3000...")
+
+	client, err := initDatabase(os.Getenv("MONGODB_URI"))
+	if err != nil {
+		panic(err)
+	}
+
+	defer endConnection(client)
 
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
 
 	tm, err := views.NewTemplateManager(templates.FS, ".", "layouts", ".html")
-
-	// error loading templates
 	if err != nil {
 		panic(err)
 	}
---
+++ services/user.go
@@ -0,0 +1,17 @@
+package services
+
+// type UserService struct {
+// 	client *mongo.Client
+// }
+
+// func NewUserService() (userService *UserService) {
+
+// }
+
+// func (s *UserService) SignIn() {
+
+// }
+
+// func (s *UserService) SignUp() {
+
+// }
